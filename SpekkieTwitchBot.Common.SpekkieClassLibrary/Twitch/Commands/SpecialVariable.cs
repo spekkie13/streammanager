302957b823223d5f7f7ca779f7351f451777fcc3
spekkie13 <32901490+spekkie13@users.noreply.github.com>
2025-03-26 18:46:30 +0100

    started putting text command handling into place

---
+++ SpekkieTwitchBot.Common.SpekkieClassLibrary/Twitch/Commands/SpecialVariable.cs
@@ -0,0 +1,7 @@
+namespace SpekkieClassLibrary.Twitch.Commands;
+
+public class SpecialVariable
+{
+    public string Name { get; set; }
+    public string Value { get; set; }
+}
--- SpekkieTwitchBot.Twitch.CommandService/CommandHandlers/TextCommandHandler.cs
+++ SpekkieTwitchBot.Twitch.CommandService/CommandHandlers/TextCommandHandler.cs
@@ -2,24 +2,87 @@
 
 public class TextCommandHandler
 {
+    /*
+     Compose a json file storing all commands and their response => TextCommands.json
+     Store any special variables (like follower count, sub count, etc.) in a separate json file => SpecialVariables.json
+     Write logic to replace any special variables with their actual values if the command response contains their names
+     
+     Figure out a way to execute functions at runtime from a json response & replace action
+     */
+    
+    private Dictionary<string, string> _Commands = new ();
+    private Dictionary<string, string> _SpecialVariables = new ();
+
+    public TextCommandHandler()
+    {
+        LoadSpecialVariables();
+        LoadTextCommands();
+    }
+
+    private void LoadTextCommands()
+    {
+        _Commands = new Dictionary<string, string>();
+        //read TextCommands.json
+        //transcribe it into a Dictionary<string, string>
+    }
+
+    private void LoadSpecialVariables()
+    {
+        _SpecialVariables = new Dictionary<string, string>();
+        //read SpecialVariables.json
+        //transcribe it into a Dictionary<string, string>
+    }
+    
+    public string HandleCommand(string key)
+    {
+        string response = _Commands.GetValueOrDefault(key, "Unknown command");
+        bool containsSpecialVariables = false;
+        foreach (string variable in _SpecialVariables.Keys)
+        {
+            if (response.Contains(variable))
+                containsSpecialVariables = true;
+        }
+
+        if (containsSpecialVariables)
+        {
+            response = response.Replace(_SpecialVariables[key], "");
+        }
+        
+        return response;
+    }
+
+    public void AddCommand(string command, string response)
+    {
+        _Commands.Add(command, response);
+    }
+
+    public void AddSpecialVariable(string variable, string response)
+    {
+        _SpecialVariables.Add(variable, response);
+    }
+    
     public static string HandleGetYouTubeCommand()
     {
+        //Added in TextCommands.json
         return "Checkout my YouTube Channel: https://youtube.com/@spekkieclashes";
     }
 
     public static string HandleGetDiscordCommand()
     {
+        //Added in TextCommands.json
         return 
             "Wanna connect off-stream? Join my discord server: https://discord.gg/8Ez2dZNxeV";
     }
 
     public static string HandleGetTwitterCommand()
     {
+        //Added in TextCommands.json
         return "Checkout my Twitter: https://twitter.com/CSpekkie";
     }
 
     public static string HandleGetCocTagCommand()
     {
+        //Added in TextCommands.json
         return "My in-game tag is #QYCCULVY";
     }
 
@@ -30,6 +93,7 @@ public class TextCommandHandler
 
     public static string HandleHelloCommand()
     {
+        //Added in TextCommands.json
         return "Hello World";
     }
 }
